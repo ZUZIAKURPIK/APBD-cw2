32e5f70d15aba3042e00dcbed51462b2ff32dbad
Zuzanna Kurpik <s27232@pjwstk.edu.pl>
2024-03-12 18:25:09 +0100

    create new method GetMax()

--- Cwiczenia 2/Cwiczenia 2/Program.cs
+++ Cwiczenia 2/Cwiczenia 2/Program.cs
@@ -1,21 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Uczę");
-Console.WriteLine("Sie");
-Console.WriteLine("GITA");
-
-static double GetAverage(int[] liczby)
+static int GetMax(int[] inty)
 {
-    int suma = 0;
-    foreach (var liczba in liczby)
-    {
-        suma += liczba;
-    }
+    int max = 0;
 
-    return (double)suma / liczby.Length;
+    foreach (var wartosc in inty)
+    {
+        if (wartosc > max) max = wartosc;
+    }
+    
+    return max;
 }
 
-int[] tablica = {3, 5, 6, 7, 1};
-double avf = GetAverage(tablica);
-Console.WriteLine(avf);
+int[] tab = { 1, 2, 3, 4, 5, 6, 7, 9999 };
+int odp = GetMax(tab);
+Console.WriteLine(odp);
