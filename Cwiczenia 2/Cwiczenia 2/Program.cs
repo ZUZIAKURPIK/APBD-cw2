d6908d6292614886fa0bef5d0573841d94a71e40
Zuzanna Kurpik <s27232@pjwstk.edu.pl>
2024-03-12 13:30:42 +0100

    add average method

--- Cwiczenia 2/Cwiczenia 2/Program.cs
+++ Cwiczenia 2/Cwiczenia 2/Program.cs
@@ -1,9 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-
 Console.WriteLine("Uczę");
-
 Console.WriteLine("Sie");
-
 Console.WriteLine("GITA");
+
+static double GetAverage(int[] liczby)
+{
+    int suma = 0;
+    foreach (var liczba in liczby)
+    {
+        suma += liczba;
+    }
+
+    double avg = (double)suma / liczby.Length;
+
+    return avg;
+}
